73286e8cea3bf9a7c1a703b82c932cca2a915b30
Astrawan Wayan <astra@pm.me>
2025-11-07 22:13:27 +0800

    feat(flake): periodic update

---
+++ modules/home-manager/features/desktop/gnome/extensions/dash-to-dock.nix
@@ -0,0 +1,22 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.devlive.features.desktop.gnome;
+in
+{
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      gnomeExtensions.dash-to-dock
+    ];
+
+    # dconf.settings."org/gnome/shell/extensions/Logo-menu" = {
+    #     # Use right click to open Activities.
+    #     menu-button-icon-click-type = 3;
+    #
+    #     # Use the NixOS logo.
+    #     menu-button-icon-image = 23;
+    #
+    #     menu-button-terminal = "ghostty";
+    # };
+  };
+}
