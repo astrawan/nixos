ae6eb2b544527e94a7aef65ffbe58cd372ad9d38
Astrawan Wayan <astra@pm.me>
2026-01-11 18:09:26 +0800

    feat(nixos): tailscale enable taildrop and add shields up option

--- modules/nixos/services/tailscale.nix
+++ modules/nixos/services/tailscale.nix
@@ -6,11 +6,12 @@ in
 {
   config = lib.mkIf cfg.enable {
     services.tailscale = {
-      disableTaildrop = true;
+      disableTaildrop = false;
       enable = true;
       extraSetFlags = [
         "--operator"
         "${config.devlive.user.name}"
+        "--shields-up"
       ];
       useRoutingFeatures = "both";
     };
